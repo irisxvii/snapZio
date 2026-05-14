a3dda0140f0a995a127e559e1ec4eb253654ffb6
iris <iriskurien@gmail.com>
2026-05-14 23:03:18 +0530

    initial setup

---
+++ app/src/main/java/com/trackzio/iris/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.trackzio.iris
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import com.trackzio.iris.ui.theme.SnapZioTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            SnapZioTheme {
+                Surface(
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Text("ahh")
+                }
+            }
+        }
+    }
+}
